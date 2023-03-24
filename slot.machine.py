63a2026fe3f726115694eca1a6cd2cc040b683dd
Khusniyor Suvanov <87824164+Suvanov04@users.noreply.github.com>
2023-03-24 23:10:00 +0500

    Update slot.machine.py

--- slot.machine.py
+++ slot.machine.py
@@ -76,9 +76,9 @@ def deposit():
 			if amount > 0:
 				break
 			else:
-				print ("Deposit amount must be greater than 0 ")
+				print ("Deposit amount must be greater than 0")
 		else:
-			print ("Please enter a number only.")
+			print ("Please enter a number only. ")
 
 	return amount
 
